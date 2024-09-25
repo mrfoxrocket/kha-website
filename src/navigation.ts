9b45a26f830ac8dc521d99f08fa66b3254a27923
Luka <159396213+mrfoxrocket@users.noreply.github.com>
2024-09-25 21:38:50 +1200

    .

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,183 +1,71 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
-  links: [
-    {
-      text: 'Homes',
-      links: [
+    links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
+            text: 'About',
+            href: '/about',
         },
         {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
+            text: 'Contact',
+            href: '/contact',
         },
         {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
+            text: 'Investors',
+            links: [
+                {
+                    text: 'Information',
+                    href: getPermalink('/invest/info'),
+                },
+                {
+                    text: 'Process',
+                    href: getPermalink('/invest/process'),
+                },
+            ],
+        },
+
+        {
+            text: 'Owners',
+            links: [
+                {
+                    text: 'Information',
+                    href: getPermalink('/owners/info'),
+                },
+                {
+                    text: 'Process',
+                    href: getPermalink('/owners/process'),
+                },
+            ],
         },
         {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
+            text: 'Exchange',
+            href: '/exchange',
         },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
-    },
-  ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+    ],
 };
 
 export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
+    links: [
+        {
+            title: 'Information',
+            links: [
+                { text: 'About', href: getPermalink('/about') },
+                { text: 'Contact', href: getPermalink('/contact') },
+                { text: 'Exchange', href: getPermalink('/exchange') },
+                { text: 'Investors', href: getPermalink('/invest') },
+                { text: 'Owners', href: getPermalink('/owners') },
+            ],
+        },
+    ],
+    secondaryLinks: [
+        { text: 'Terms', href: getPermalink('/terms') },
+        { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    ],
+    socialLinks: [
+        // { ariaLabel: 'X', icon: 'tabler:brand-x', href: getPermalink('/owners/process') },
+        // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: getPermalink('/owners/process') },
+        // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: getPermalink('/owners/process') },
+        // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+        // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,64 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import typographyPlugin from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import typographyPlugin from "@tailwindcss/typography";
 
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: 'var(--aw-color-primary)',
-        secondary: 'var(--aw-color-secondary)',
-        accent: 'var(--aw-color-accent)',
-        default: 'var(--aw-color-text-default)',
-        muted: 'var(--aw-color-text-muted)',
-      },
-      fontFamily: {
-        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
-        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            animation: {
+                wiggle: "wiggle 2s ease-in-out infinite",
+                slowspin: "spin 5s linear infinite",
+            },
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-2deg)" },
+                    "50%": { transform: "rotate(2deg)" },
+                },
+            },
+            colors: {
+                primary: "hsl(var(--primary))",
+                secondary: "hsl(var(--secondary))",
+                accent: "hsl(var(--accent))",
+                default: "hsl(var(--aw-color-text-default))",
+                background: "hsl(var(--background))",
+                accentbackground: "hsl(var(--accent-background))",
+                foreground: "hsl(var(--foreground))",
+                card: {
+                    DEFAULT: "hsl(var(--card))",
+                    foreground: "hsl(var(--card-foreground))",
+                },
+                popover: {
+                    DEFAULT: "hsl(var(--popover))",
+                    foreground: "hsl(var(--popover-foreground))",
+                },
+
+                muted: {
+                    DEFAULT: "hsl(var(--muted))",
+                    foreground: "hsl(var(--muted-foreground))",
+                },
+
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive))",
+                    foreground: "hsl(var(--destructive-foreground))",
+                },
+                border: "hsl(var(--border))",
+                input: "hsl(var(--input))",
+                ring: "hsl(var(--ring))",
+                chart: {
+                    1: "hsl(var(--chart-1))",
+                    2: "hsl(var(--chart-2))",
+                    3: "hsl(var(--chart-3))",
+                    4: "hsl(var(--chart-4))",
+                    5: "hsl(var(--chart-5))",
+                },
+            },
+            fontFamily: {
+                sans: ["var(--aw-font-sans, ui-sans-serif)", ...defaultTheme.fontFamily.sans],
+                serif: ["var(--aw-font-serif, ui-serif)", ...defaultTheme.fontFamily.serif],
+                heading: ["var(--aw-font-heading, ui-sans-serif)", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [typographyPlugin],
-  darkMode: 'class',
+    plugins: [typographyPlugin],
+    darkMode: "class",
 };

03cdeeaf0792f58d8a13659d9dfad53a50da6c0b
Luka <159396213+mrfoxrocket@users.noreply.github.com>
2024-09-29 13:59:31 +1300

    .

--- src/navigation.ts
+++ src/navigation.ts
@@ -23,14 +23,10 @@ export const headerData = {
                 },
             ],
         },
-
         {
             text: "Owners",
             links: [
-                {
-                    text: "Information",
-                    href: getPermalink("/owners/info"),
-                },
+                { text: "Information", href: getPermalink("/owners") },
                 {
                     text: "Process",
                     href: getPermalink("/owners/process"),
@@ -62,9 +58,7 @@ export const footerData = {
         { text: "Privacy Policy", href: getPermalink("/privacy") },
     ],
     socialLinks: [
-        // { ariaLabel: 'X', icon: 'tabler:brand-x', href: getPermalink('/owners/process') },
-        // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: getPermalink('/owners/process') },
-        // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: getPermalink('/owners/process') },
+        // { ariaLabel: 'X', icon: 'tabler:brand-x', href: getPermalink('/owners/process') }, // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: getPermalink('/owners/process') }, // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: getPermalink('/owners/process') },
         // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
         // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
     ],
